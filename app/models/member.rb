0c19fe000032c493ab5f1dcb1a63efbf8bf9b165
Laura Berge <lauraeberge@gmail.com>
2019-09-27 02:22:11 +0000

    Automatically backed up by Learn

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,17 +1,4 @@
-class Member #< ActiveRecord::Base
-  #belongs_to :team
-  attr_accessor :name, :power, :bio, :team
-  @@all = []
-  
-  def initialize(name:, power:, bio:)
-    @name = name
-    @power = power
-    @bio = bio
-    @@all << self
-  end
-  
-  def self.all
-    @@all
-  end
-  
+class Member < ActiveRecord::Base
+  belongs_to :team
+
 end
