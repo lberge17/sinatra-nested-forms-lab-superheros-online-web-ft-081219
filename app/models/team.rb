bb18aeb37cb9fbdc7a7763640899b6c9e908e73c
Laura Berge <lauraeberge@gmail.com>
2019-09-27 02:14:48 +0000

    Automatically backed up by Learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,11 @@
-class Team < ActiveRecord::Base
-  has_many :members
+class Team #< ActiveRecord::Base
+  #has_many :members
+  attr_accessor :name, :motto
+  @@all = []
+  
+  def initialize(:name, :motto)
+    @name = name
+    @motto = motto
+    @@all << self
+  end
 end
